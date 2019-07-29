845374e7430f469b2bb8ddaf933dd158be953c3e
s_ursimr <s_ursimr@BRYNNER.ENCS.concordia.ca>
2019-07-29 15:59:36 -0400

    fe implementation

--- ReplicaManager1FEandClient/src/CommonUtils/CommonUtils.java
+++ ReplicaManager1FEandClient/src/CommonUtils/CommonUtils.java
@@ -17,6 +17,79 @@ import java.util.logging.SimpleFormatter;
  * @author Natheepan Ganeshamoorthy
  */
 public class CommonUtils {
+	
+	//CONSTANTS
+	public static final String REPLICA1_HOSTNAME="SENEGAL";//
+	public static final String REPLICA2_HOSTNAME="GHANA"; //
+	public static final String FRONT_END_HOSTNAME="GHANA"; // 
+	public static final String REPLICA3_HOSTNAME="GUINEA";// 
+	public static final String SEQUENCER_HOSTNAME="GUINEA";//
+	public static final String REPLICA4_HOSTNAME="LIBERIA";//
+	
+	
+//	public static final String REPLICA1_IPADDRESS="132.205.4.148";
+//	public static final String REPLICA2_IPADDRESS="132.205.4.149";
+//	public static final String REPLICA3_IPADDRESS="132.205.4.152";
+//	public static final String SEQUENCER_IPADDRESS="132.205.4.152";
+//	public static final String REPLICA4_IPADDRESS="";
+	
+	public static final int TO_REPLICA_STRING_PORT = 1212;
+	public static final int SEQUNECER_PORT=1218;
+	public static final int MULTICAST_PORT = 1219;
+	public static final int FRONT_END_PORT = 1220;
+	public static final int RECEIVE_DATA_FROM_REPLICA_PORT = 2010;
+	
+	public final static String INET_ADDR = "224.0.0.3";
+	public final static int PORT = 8888;
+
+	
+	public static final String ADD_EVENT = "ADD_EVENT";
+	public static final String REMOVE_EVENT = "REMOVE_EVENT";
+	public static final String LIST_EVENT = "LIST_EVENT";
+	public static final String eventAvailable = "eventAvailable";
+	public static final String validateBooking = "validateBooking";
+	public static final String BOOK_EVENT = "BOOK_EVENT";
+	public static final String CANCEL_EVENT = "CANCEL_EVENT";
+	public static final String SWAP_EVENT = "SWAP_EVENT";
+	public static final String NON_OriginCustomerBooking = "NON_ORIGIN_CUSTOMER_BOOKING";
+	public static final String GET_DATA = "GET_BOOKING_SCHEDULE";
+	
+
+	public static final String SOME_THING_WENT_WRONG = "Something went wrong. Please try again!";
+	
+	
+	public static final String CRASHED = "CRASHED";
+	public static final String FRONT_END = "FRONT_END";
+	public static final String THREE_CONSECUTIVE_ERRORS = "Three Consecutive Errors";
+	public static final String RESULT_ERROR = "Result Error";
+	public static final int REPLICA_CONCORDIA_SERVER_PORT = 2000;
+	public static final int REPLICA_MCGILL_SERVER_PORT = 2002;
+	public static final int REPLICA_MONTREAL_SERVER_PORT = 2004;
+	public static final int REPLICA_TO_REPLICA_PORT = 2006;
+	
+	public static final String CONCORDIA_CODE = "CON";
+	public static final String MCGILL_CODE = "MCG";
+	public static final String MONTREAL_CODE = "MON";
+	public static final String I_AM_ALIVE = "I am Alive";
+	public static String USER_CODE = "U";
+	public static String MANAGER_CODE = "M";
+	public static String LOGGER_FOLDER="/logs";
+	public static String CONCORDIA_INITAL_LOAD_FILE = "/resources/ConcordiaLibrary.txt";
+	public static String MCGGILL_INITAL_LOAD_FILE = "/resources/McgillLibrary.txt";
+	public static String MONTREAL_INITAL_LOAD_FILE = "/resources/MontrealLibrary.txt";
+	public static String CONCORDIA_SERVER_LOG_FILE = "/log/concordia_server_log.log";
+	public static String MCGILL_SERVER_LOG_FILE = "/log/mcgill_server_log.log";
+	public static String MONTREAL_SERVER_LOG_FILE = "/log/montreal_server_log.log";
+	public static String LOG_FOLDER = "/log/";
+	public static String CONCORDIA_SERVER = "CONCORDIA_SERVER";
+	public static String MCGILL_SERVER = "MCGILL_SERVER";
+	public static String MONTREAL_SERVER = "MONTREAL_SERVER";
+	public static String WAITING_LIST_MESSAGE="Book not available. Do you want to be added in waiting list?(y/n)";
+	public static int CONCORDIA_SERVER_PORT = 5555;
+	public static int MCGILL_SERVER_PORT = 6666;
+	public static String TRUE = "true";
+	public static String FALSE = "false";
+	///////////////////////////////////////////////
 
     public static final String CUSTOMER_ClientType = "C";
     public static final String EVENT_MANAGER_ClientType = "M";
---
+++ ReplicaManager1FEandClient/src/Model/MessageData.java
@@ -0,0 +1,140 @@
+package Model;
+
+import java.io.Serializable;
+
+public class MessageData implements Serializable {
+
+	private String customerIDName;
+	private String managerIDName;
+	private String eventTypeName;
+	private String eventIDName;
+	private String bookingCap;
+	private String managerID;
+	private  String new_EventID;
+	private  String old_EventID;
+	
+	
+	public String getOld_EventID() {
+		return old_EventID;
+	}
+
+	public MessageData setOld_EventID(String old_EventID) {
+		this.old_EventID = old_EventID;
+		return this;
+	}
+
+	public String getOld_EventType() {
+		return old_EventType;
+	}
+
+	public MessageData setOld_EventType(String old_EventType) {
+		this.old_EventType = old_EventType;
+		return this;
+	}
+
+	private  String old_EventType;
+    private String new_EventType;
+	private String methodName;
+	private boolean isFromOtherServer;
+	private int sequenceCounter;
+	private int errorCounter;
+	
+	public int getErrorCounter() {
+		return errorCounter;
+	}
+
+	public void setErrorCounter(int errorCounter) {
+		this.errorCounter = errorCounter;
+	}
+
+	public String getNewEventId() {
+		return new_EventID;
+	}
+
+	public MessageData setNewEventId(String newItemId) {
+		this.new_EventID = newItemId;
+		return this;
+	}
+	public String getNewEventType() {
+		return new_EventType;
+	}
+
+	public MessageData setNewEventType(String newItemId) {
+		this.new_EventType = newItemId;
+		return this;
+	}
+
+	public boolean isFromOtherServer() {
+		return isFromOtherServer;
+	}
+
+	public MessageData setFromOtherServer(boolean isFromOtherServer) {
+		this.isFromOtherServer = isFromOtherServer;
+		return this;
+	}
+
+	public String getCustomerId() {
+		return customerIDName;
+	}
+	public String getManagerId() {
+		return managerIDName;
+	}
+
+	public String getEventId() {
+		return eventIDName;
+	}
+
+	public String getEventType() {
+		return eventTypeName;
+	}
+
+
+	
+	public String getBookingCap() {
+		return bookingCap;
+	}
+
+
+	public String getMethodName() {
+		return methodName;
+	}
+
+	public int getSequenceCounter() {
+		return sequenceCounter;
+	}
+
+	public void setSequenceCounter(int sequenceCounter) {
+		this.sequenceCounter = sequenceCounter;
+	}
+
+	public MessageData setCustomerId(String userId) {
+		this.customerIDName = userId;
+		return this;
+	}
+
+	public MessageData setManagerId(String userId) {
+		this.managerIDName = userId;
+		return this;
+	}
+
+	public MessageData setEventId(String itemId) {
+		this.eventIDName = itemId;
+		return this;
+	}
+
+	public MessageData setEventType(String itemName) {
+		this.eventIDName = itemName;
+		return this;
+	}
+
+	public MessageData setBookingCap(String quantity) {
+		this.bookingCap = quantity;
+		return this;
+	}
+
+	public MessageData setMethodName(String methodName) {
+		this.methodName = methodName;
+		return this;
+	}
+	
+}
