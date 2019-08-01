f9e56f8f56a20fdfef95c62669972dff9ba689c2
gursimransingh <gursimran81@gmail.com>
2019-07-31 20:33:21 -0400

    updates

---
+++ Replica3/src/CommonUtils/CommonUtils.java
@@ -0,0 +1,169 @@
+package CommonUtils;
+
+/**
+ * @author gursimransingh
+ */
+/**
+ * CONCORDIA UNIVERSITY
+ * DEPARTMENT OF COMPUTER SCIENCE AND SOFTWARE ENGINEERING
+ * COMP 6231, Summer 2019 Instructor: Sukhjinder K. Narula
+ * ASSIGNMENT 1
+ * Issued: May 14, 2019 Due: Jun 3, 2019
+ */
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ *
+ * @author Natheepan Ganeshamoorthy
+ */
+public class CommonUtils {
+
+    //CONSTANTS
+    public static final String REPLICA1_HOSTNAME="BURSTYN";//
+    public static final String REPLICA2_HOSTNAME="BRYNNER"; //
+    public static final String FRONT_END_HOSTNAME="BURSTYN"; //
+    public static final String REPLICA3_HOSTNAME="GUINEA";//
+    public static final String SEQUENCER_HOSTNAME="BRYNNER";//
+
+
+	public static final String REPLICA1_IPADDRESS="132.205.4.27";
+//	public static final String REPLICA2_IPADDRESS="132.205.4.149";
+//	public static final String REPLICA3_IPADDRESS="132.205.4.152";
+	public static final String SEQUENCER_IPADDRESS="132.205.4.26";
+//	public static final String REPLICA4_IPADDRESS="";
+
+    public static final int TO_REPLICA_STRING_PORT = 1212;
+    public static final int SEQUNECER_PORT=1218;
+    public static final int MULTICAST_PORT = 1219;
+    public static final int FRONT_END_PORT = 1220;
+    public static final int RECEIVE_DATA_FROM_REPLICA_PORT = 2010;
+
+    public final static String INET_ADDR = "224.0.0.3";
+    public final static int PORT = 8888;
+
+
+    public static final String ADD_EVENT = "ADD_EVENT";
+    public static final String REMOVE_EVENT = "REMOVE_EVENT";
+    public static final String LIST_EVENT = "LIST_EVENT";
+    public static final String eventAvailable = "eventAvailable";
+    public static final String validateBooking = "validateBooking";
+    public static final String BOOK_EVENT = "BOOK_EVENT";
+    public static final String CANCEL_EVENT = "CANCEL_EVENT";
+    public static final String SWAP_EVENT = "SWAP_EVENT";
+    public static final String NON_OriginCustomerBooking = "NON_ORIGIN_CUSTOMER_BOOKING";
+    public static final String GET_DATA = "GET_DATA";
+    public static final String GET_BOOKING_SCHEDULE = "GET_BOOKING_SCHEDULE";
+
+
+    public static final String SOME_THING_WENT_WRONG = "Something went wrong. Please try again!";
+
+
+    public static final String CRASHED = "CRASHED";
+    public static final String FRONT_END = "FRONT_END";
+    public static final String THREE_CONSECUTIVE_ERRORS = "Three Consecutive Errors";
+    public static final String RESULT_ERROR = "Result Error";
+    public static final int REPLICA_OTTAWA_SERVER_PORT = 2000;
+    public static final int REPLICA_TORONTO_SERVER_PORT = 2002;
+    public static final int REPLICA_MONTREAL_SERVER_PORT = 2004;
+    public static final int REPLICA_TO_REPLICA_PORT = 2006;
+
+    public static final String I_AM_ALIVE = "I am Alive";
+    public static String USER_CODE = "C";
+    public static String MANAGER_CODE = "M";
+    public static String LOGGER_FOLDER="/logs";
+    public static String CONCORDIA_INITAL_LOAD_FILE = "/resources/ConcordiaLibrary.txt";
+    public static String MCGGILL_INITAL_LOAD_FILE = "/resources/McgillLibrary.txt";
+    public static String MONTREAL_INITAL_LOAD_FILE = "/resources/MontrealLibrary.txt";
+    public static String CONCORDIA_SERVER_LOG_FILE = "/log/concordia_server_log.log";
+    public static String MCGILL_SERVER_LOG_FILE = "/log/mcgill_server_log.log";
+    public static String MONTREAL_SERVER_LOG_FILE = "/log/montreal_server_log.log";
+    public static String LOG_FOLDER = "/log/";
+    public static String CONCORDIA_SERVER = "CONCORDIA_SERVER";
+    public static String MCGILL_SERVER = "MCGILL_SERVER";
+    public static String MONTREAL_SERVER = "MONTREAL_SERVER";
+    public static String WAITING_LIST_MESSAGE="Book not available. Do you want to be added in waiting list?(y/n)";
+    public static int CONCORDIA_SERVER_PORT = 5555;
+    public static int MCGILL_SERVER_PORT = 6666;
+    public static String TRUE = "true";
+    public static String FALSE = "false";
+    ///////////////////////////////////////////////
+
+    public static final String CUSTOMER_ClientType = "C";
+    public static final String EVENT_MANAGER_ClientType = "M";
+
+    public static final String MORNING = "M";
+    public static final String EVENING = "E";
+    public static final String AFTERNOON = "A";
+
+    public static final String TORONTO = "TOR";
+    public static final String MONTREAL = "MTL";
+    public static final String OTTAWA = "OTW";
+
+    public static final String TORONTO_SERVER_NAME = "TORONTO";
+    public static final String MONTREAL_SERVER_NAME = "MONTREAL";
+    public static final String OTTAWA_SERVER_NAME = "OTTAWA";
+
+    public static final int TORONTO_SERVER_PORT = 1111;
+    public static final int MONTREAL_SERVER_PORT = 2222;
+    public static final int OTTAWA_SERVER_PORT = 3333;
+
+    public static final String CONFERENCE = "Conferences";
+    public static final String SEMINAR = "Seminars";
+    public static final String TRADESHOW = "TradeShows";
+
+    public static final String OPERATIONFAILURE = "Operation Failure";
+
+    public static enum InputType
+    {
+        CLIENT_ID, EVENT_ID
+    };
+
+    public static void addFileHandler(Logger log, String fileName) throws SecurityException, IOException
+    {
+        log.setUseParentHandlers(false);
+        FileHandler fileHandler = new FileHandler(System.getProperty("user.dir") + "/Records/" + fileName + ".log", true);
+        log.addHandler(fileHandler);
+        fileHandler.setFormatter(new SimpleFormatter());
+    }
+
+    public static boolean isInputValid(String id, CommonUtils.InputType type)
+    {
+        if(type == CommonUtils.InputType.CLIENT_ID)
+        {
+            String serverId   = id.substring(0, 3);
+            String clientType = id.substring(3, 4);
+            String clientID   = id.substring(4, 8);
+
+            return id.length() == 8 && (clientType.equals(CUSTOMER_ClientType) || clientType.equals(EVENT_MANAGER_ClientType))
+                    && (serverId.equals(TORONTO) || serverId.equals(MONTREAL) || serverId.equals(OTTAWA))
+                    && (clientID.matches("^[0-9]+$"));
+        }
+
+        if(type == CommonUtils.InputType.EVENT_ID)
+        {
+            String serverId  = id.substring(0, 3);
+            String eventType = id.substring(3, 4);
+            String day       = id.substring(4, 6);
+            String month     = id.substring(6, 8);
+            String year      = id.substring(8, 10);
+
+            return id.length() == 10 && (eventType.equals(MORNING) || eventType.equals(EVENING) || eventType.equals(AFTERNOON))
+                    && (serverId.equals(TORONTO) || serverId.equals(MONTREAL) || serverId.equals(OTTAWA))
+                    && (day.length() == 2 && day.matches("0[1-9]|[1-2][0-9]|3[0-1]"))
+                    && (month.length() == 2 && month.matches("0[1-9]|1[0-2]"))
+                    && (year.length() == 2 && year.matches("19|[2-9][0-9]"));
+        }
+        return false;
+    }
+
+    public static String capitalize(String input)
+    {
+        return input.toUpperCase();
+    }
+
+}
+
---
+++ Replica3/src/Server/MontrealServer.java
@@ -0,0 +1,158 @@
+/**
+ * CONCORDIA UNIVERSITY
+ * DEPARTMENT OF COMPUTER SCIENCE AND SOFTWARE ENGINEERING
+ * COMP 6231, Summer 2019 Instructor: Sukhjinder K. Narula
+ * ASSIGNMENT 1
+ * Issued: May 14, 2019 Due: Jun 3, 2019
+ */
+package Server;
+
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.List;
+
+import CommonUtils.CommonUtils;
+import Model.EventData;
+import Model.MessageData;
+import ServerImpl.MontrealServerImpl;
+
+/**
+ *
+ * @author Gursimran Singh
+ */
+public class MontrealServer {
+
+    public static void main(String[] args)
+    {
+    	MontrealServerImpl montrealLibraryImpl = new MontrealServerImpl();
+
+		if(args.length > 0) {
+			Runnable dataConsistentImpl = () ->{
+				receiveDataConsistence(montrealLibraryImpl);
+			};
+			new Thread(dataConsistentImpl).start();
+		}
+
+		Runnable libraryServerRunnable = () ->{
+			handlesRequestFromAnotherServers(montrealLibraryImpl);
+		};
+		Runnable replicaManagerImpl = () ->{
+			handleReplicaRequests(montrealLibraryImpl,args);
+		};
+
+		new Thread(replicaManagerImpl).start();
+		new Thread(libraryServerRunnable).start();
+    }
+    private static void handleReplicaRequests(MontrealServerImpl montrealLibraryImpl, String[] args) {
+		try(DatagramSocket socket = new DatagramSocket(CommonUtils.REPLICA_MONTREAL_SERVER_PORT)) {
+			System.out.println("Montreal Server started...");
+			while(true) {
+				byte [] message = new byte[1024];
+				DatagramPacket recievedDatagramPacket = new DatagramPacket(message, message.length);
+				socket.receive(recievedDatagramPacket);
+				ObjectInputStream inputStream = new ObjectInputStream(new ByteArrayInputStream(recievedDatagramPacket.getData()));
+				MessageData messageData = (MessageData) inputStream.readObject();
+				inputStream.close();
+				byte[] byteArray = replicaManagerImpl(messageData, montrealLibraryImpl);
+				DatagramPacket reply = new DatagramPacket(byteArray, byteArray.length, recievedDatagramPacket.getAddress(),
+						recievedDatagramPacket.getPort());
+				socket.send(reply);
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static byte[] replicaManagerImpl(MessageData messageData, MontrealServerImpl montrealLibraryImpl) {
+		String response = "";
+
+		switch(messageData.getMethodName()) {
+
+		case CommonUtils.ADD_EVENT:
+			response = montrealLibraryImpl.addEvent(messageData.getEventId(), messageData.getEventType(), messageData.getBookingCap(), messageData.getManagerId());
+			break;
+		case CommonUtils.REMOVE_EVENT:
+			response = montrealLibraryImpl.removeEvent(messageData.getEventId(), messageData.getEventType(), messageData.getManagerId());
+			break;
+		case CommonUtils.LIST_EVENT:
+			response=montrealLibraryImpl.listEventAvailability(messageData.getEventType(), messageData.getManagerId());
+			break;
+		case CommonUtils.BOOK_EVENT:
+			response=montrealLibraryImpl.bookEvent(messageData.getCustomerId(), messageData.getEventId(), messageData.getEventType(), messageData.getBookingCap());
+			break;
+		case CommonUtils.GET_BOOKING_SCHEDULE:
+			response=montrealLibraryImpl.getBookingSchedule(messageData.getCustomerId(), messageData.getManagerId());
+			break;
+		case CommonUtils.GET_DATA:
+			EventData eventData = montrealLibraryImpl.getEventData();
+			try {
+				ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+				ObjectOutput objectOutput = new ObjectOutputStream(byteStream);
+				objectOutput.writeObject(eventData);
+				return byteStream.toByteArray();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+				break;
+		case CommonUtils.CANCEL_EVENT:
+			response = montrealLibraryImpl.cancelEvent(messageData.getCustomerId(), messageData.getEventId(), messageData.getEventType());
+			break;
+		case CommonUtils.NON_OriginCustomerBooking:
+			response=montrealLibraryImpl.nonOriginCustomerBooking(messageData.getCustomerId(), messageData.getEventId());
+			break;
+		case CommonUtils.SWAP_EVENT:
+			response = montrealLibraryImpl.swapEvent(messageData.getCustomerId(), messageData.getNewEventId(), messageData.getNewEventType(), messageData.getOld_EventID(), messageData.getOld_EventType());
+			break;
+		case CommonUtils.CRASHED:
+			response = CommonUtils.I_AM_ALIVE;
+			break;
+		case CommonUtils.eventAvailable:
+			response = montrealLibraryImpl.eventAvailable(messageData.getEventId(), messageData.getEventType());
+			break;
+		case CommonUtils.validateBooking:
+			response = montrealLibraryImpl.validateBooking(messageData.getCustomerId(), messageData.getEventId(), messageData.getEventType());
+		default: 
+			response="Invalid request!!!";
+		}
+		return response.getBytes();
+
+	}
+
+	private static void handlesRequestFromAnotherServers(MontrealServerImpl montrealLibraryImpl){
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket(CommonUtils.MONTREAL_SERVER_PORT);
+			System.out.println("Montreal Server started...");
+			while(true) {
+				byte [] message = new byte[1000];
+				DatagramPacket recievedDatagramPacket = new DatagramPacket(message, message.length);
+				socket.receive(recievedDatagramPacket);
+				String response = montrealLibraryImpl.handleRequestFromOtherServer(new String(recievedDatagramPacket.getData()));
+				DatagramPacket reply = new DatagramPacket(response.getBytes(), response.length(), recievedDatagramPacket.getAddress(),
+						recievedDatagramPacket.getPort());
+				socket.send(reply);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(socket != null)
+				socket.close();
+		}
+	}
+
+	private static void receiveDataConsistence(MontrealServerImpl montrealLibraryImpl) {
+		try(DatagramSocket socket = new DatagramSocket(CommonUtils.RECEIVE_DATA_FROM_REPLICA_PORT)){
+			byte [] message = new byte[1024];
+			DatagramPacket recievedDatagramPacket = new DatagramPacket(message, message.length);
+			socket.receive(recievedDatagramPacket);
+			ObjectInputStream inputStream = new ObjectInputStream(new ByteArrayInputStream(recievedDatagramPacket.getData()));
+			EventData eventData = (EventData) inputStream.readObject();
+			montrealLibraryImpl.parseEventnfo(eventData);
+			inputStream.close();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
